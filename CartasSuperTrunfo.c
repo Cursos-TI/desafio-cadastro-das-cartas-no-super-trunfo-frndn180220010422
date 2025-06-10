60af4ed7c48a7c8d69948302e7fc3739e51f7c6f
frndn180220010422 <fernandojunior1802@gmail.com>
2025-06-10 09:57:12 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 
 int main() {
-    char estado1, estado2;
-    char codigo1, codigo2;
+    char estado1[3], estado2[3];
+    char codigo1[10], codigo2[10];
     char cidade1[50], cidade2[50];
     int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int pontos1, pontos2;
 
-    printf("Carta 1\n");
-    printf("Digite o estado (sigla): ");
-    scanf(" %c", &estado1);
-    printf("Digite o codigo da carta: ");
-    scanf(" %c", &codigo1);
-    printf("Digite o nome da cidade: ");
-    scanf(" %s", cidade1);
-    printf("Digite a populacao: ");
+    printf("Carta 1\n\n");
+
+    printf("Estado: ");
+    scanf("%s", estado1);
+    printf("Código: ");
+    scanf("%s", codigo1);
+    printf("Nome da cidade: ");
+    scanf("%s", cidade1);
+    printf("População: ");
     scanf("%d", &populacao1);
-    printf("Digite a area em km2: ");
+    printf("Área: ");
     scanf("%f", &area1);
-    printf("Digite o PIB: ");
+    printf("PIB: ");
     scanf("%f", &pib1);
-    printf("Digite o numero de pontos turisticos: ");
+    printf("Número de pontos turísticos: ");
     scanf("%d", &pontos1);
 
-    printf("\nCarta 2\n");
-    printf("Digite o estado (sigla): ");
-    scanf(" %c", &estado2);
-    printf("Digite o codigo da carta: ");
-    scanf(" %c", &codigo2);
-    printf("Digite o nome da cidade: ");
-    scanf(" %s", cidade2);
-    printf("Digite a populacao: ");
+    printf("\nCarta 2\n\n");
+
+    printf("Estado: ");
+    scanf("%s", estado2);
+    printf("Código: ");
+    scanf("%s", codigo2);
+    printf("Nome da cidade: ");
+    scanf("%s", cidade2);
+    printf("População: ");
     scanf("%d", &populacao2);
-    printf("Digite a area em km2: ");
+    printf("Área: ");
     scanf("%f", &area2);
-    printf("Digite o PIB: ");
+    printf("PIB: ");
     scanf("%f", &pib2);
-    printf("Digite o numero de pontos turisticos: ");
+    printf("Número de pontos turísticos: ");
     scanf("%d", &pontos2);
 
     printf("\nCartas inseridas com sucesso!\n");
